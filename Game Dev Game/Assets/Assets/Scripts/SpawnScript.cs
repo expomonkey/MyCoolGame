2f76ea686a0c285c6bad08f004401d2635ce57c2
Unknown <sap565@nyu.edu>
2017-03-08 04:27:12 -0500

    Adding All My Project Files

---
+++ Game Dev Game/Assets/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+    public GameObject[] obj;
+    public float spawntime = 8.5f;
+    public float CameraSpeed=0.02f;
+    //public float thing = obj[0].x;
+	// Use this for initialization
+	void Start () {
+        spawn();
+	}
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.y += CameraSpeed;
+        transform.position = pos;
+    }
+    
+	void spawn()
+    {
+        GameObject thing = obj[Random.Range(0, obj.GetLength(0))];
+        Vector3 pos = thing.transform.position;
+        pos.y = pos.y+transform.position.y;
+        Instantiate(thing, pos, Quaternion.identity);
+        //GameObject thing =Instantiate(obj[Random.Range(0, obj.GetLength(0))],transform.position,Quaternion.identity);
+        // Vector3 thingpos = thing.transform.position;
+        // thingpos.x = 0.0f;
+        // thingpos.y = 12.0f;
+        //  thing.transform.position = thingpos;
+
+        Invoke("spawn", spawntime);
+    }
+}
---
+++ Game Dev Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+    public float CameraSpeed=.02f;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+          Vector3 campos = transform.position;
+        campos.y += CameraSpeed;
+          //campos.y = player.transform.position.y+offset.y;
+          transform.position = campos;
+        //transform.position.y = player.transform.position.y + offset.y;
+    }
+}
